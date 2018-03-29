db87c4c9c960d4c4d71fa3a1e4b4578cc09b2668
boyasravanireddy <sravanishilpa717@gmail.com>
2018-03-29 14:01:36 +0530

    add media query

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function loadjson(file,callback)
 var xhr=new XMLHttpRequest();
 xhr.overrideMimeType("application/json");
 xhr.open("GET",file,true);
-xhr.onreadystatechange=function()
+xhr.onreadystatechange=function ()
 {
   if(xhr.readyState ===4 && xhr.status == "200")
   {
@@ -15,20 +15,24 @@ xhr.onreadystatechange=function()
 xhr.send();
 }
 // function calling
-loadjson("data.json",function(text){
+loadjson("data.json",function (text){
 let data=JSON.parse(text);
+
 console.log(data);
+
 basics(data.left);
+education(data.education);
+skill(data.skills);
 })
 // for main div calling
-// var main=document.queryselector('.main');
+// var main=document.queryselector('.main')
 // for main div id calling
 var main=document.getElementById('main');
 var left=document.createElement("div");
 // add class name to element
 left.classList.add("left");
 // add id to element
-left.setAttribute("id","left");
+//left.setAttribute("id","left");
 // adding text to div
 left.textContent="Profile Details:"
 left.appendChild(document.createElement("HR"));
@@ -48,4 +52,44 @@ function basics(leftside){
   ph.textContent=leftside.phone;
   email.appendChild(ph);
   console.log(left);
+}
+var right=document.createElement("div");
+right.classList.add("right");
+main.appendChild(right);
+// education div start
+var edu=document.createElement("div");
+edu.classList.add("eb")
+edu.textContent="Education Details";
+edu.appendChild(document.createElement("HR"));
+right.appendChild(edu);
+function education(Educa){
+  for(i in Educa){
+    var e1=document.createElement("h1");
+    e1.classList.add("edu2");
+    e1.textContent=Educa[i].course;
+    var ul=document.createElement("ul");
+    for(j in Educa[i].college){
+      var li=document.createElement("li");
+      li.textContent=Educa[i].college[j];
+      ul.appendChild(li);
+      e1.appendChild(ul);
+      edu.appendChild(e1);
+    }
+  }
+
+}
+function skill(skills){
+  var skill_title=document.createElement("div");
+  skill_title.classList.add("tech_skill");
+  skill_title.textContent="Technical skills:";
+  skill_title.appendChild(document.createElement("HR"));
+right.appendChild(skill_title);
+// table creation
+var table=document.createElement("table");
+var row="";
+for(var i = 0;i < skills.length; i++){
+  row =row+"<tr><td>"+skills[i].name+"</td><td>"+skills[i].value+"</td></tr>"
+}
+table.innerHTML=row;
+skill_title.appendChild(table);
 }
